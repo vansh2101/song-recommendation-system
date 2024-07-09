1f753ff4bb4e3fff95b5a6ce234d7d4f09c4aa00
Vansh Sachdeva <vansh2101sachdeva@gmail.com>
2024-07-09 23:44:30 +0530

    changed the similarity function

--- utils/similarity_score.py
+++ utils/similarity_score.py
@@ -9,14 +9,17 @@ def cosine(v1, v2):
 
 def match(v1, v2):
     if isinstance(v1, list) and isinstance(v2, list):
-        scores = []
-        for i in range(len(v1)):
-            scores.append(int(v1[i] == v2[i]))
+        unique = set(v1).intersection(v2)
+        num = len(v1) + len(v2) - len(unique)
 
-        return sum(scores) / len(scores)
+        return len(unique) / num
 
     return int(v1 == v2)
 
 
-def manhattan_distance(v1, v2):
-    return 1 - np.sum(np.abs(v1 - v2))
+def manhattan_distance(v1, v2, scale=1):
+    return 1 - (np.sum(np.abs(v1 - v2)) / scale)
+
+
+def inverse_manhattan_distance(v1, v2, scale=100):
+    return 1 / (1 + (np.sum(np.abs(v1 - v2) / scale)))
